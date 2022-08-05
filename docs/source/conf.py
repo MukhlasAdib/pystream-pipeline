b7483a2e2d141fa7eeb9632ad0447b19719991bf
Mukhlas Adib <45409496+MukhlasAdib@users.noreply.github.com>
2022-08-05 18:59:49 +0700

    🔖 v0.1.1 (#4)
    increment version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2022, Mukhlas Adib"
 author = "Mukhlas Adib"
 
 # The full version, including alpha/beta/rc tags
-release = "v0.1.0"
+release = "v0.1.1"
 
 
 # -- General configuration ---------------------------------------------------
--- pystream/__init__.py
+++ pystream/__init__.py
@@ -2,4 +2,4 @@ from pystream import functional
 from pystream.pipeline.pipeline import Pipeline
 from pystream.stage.stage import Stage
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
