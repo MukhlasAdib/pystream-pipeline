c90d262af1771a9a6856a0da216307d3075ec5ff
Mukhlas Adib <adib.rasyidy@gmail.com>
2023-06-14 12:02:55 +0700

    change main pipeline name

--- pystream/utils/general.py
+++ pystream/utils/general.py
@@ -3,5 +3,5 @@ from pathlib import Path
 import pystream
 
 _PYSTREAM_DIR = str(Path(pystream.__file__).parent.absolute())
-_PIPELINE_NAME_IN_PROFILE = "Pipeline"
+_PIPELINE_NAME_IN_PROFILE = "MainPipeline"
 _FINAL_STAGE_NAME = "FinalStage"
