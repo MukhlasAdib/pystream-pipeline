b87f45a2d4465987e10044a39258c633a92db8eb
Mukhlas Adib <adib.rasyidy@gmail.com>
2023-06-19 17:24:42 +0700

    docstrings

--- pystream/utils/general.py
+++ pystream/utils/general.py
@@ -12,9 +12,20 @@ _PROFILER_DB_FOLDER = os.path.join(_PYSTREAM_DIR, "user_data")
 
 
 def set_profiler_db_folder(folder_path: str) -> None:
+    """Set the folder for the profiler SQLite database.
+    This should be called before the Pipeline instantiation
+
+    Args:
+        folder_path (str): path to the folder
+    """
     global _PROFILER_DB_FOLDER
     _PROFILER_DB_FOLDER = folder_path
 
 
 def get_profiler_db_folder() -> str:
+    """Get the folder for the profiler SQLite database
+
+    Returns:
+        str: the DB folder
+    """    
     return _PROFILER_DB_FOLDER
