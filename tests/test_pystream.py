c2e915bd5612f74e8e773262dae7e8cbb13625e7
Mukhlas Adib <45409496+MukhlasAdib@users.noreply.github.com>
2023-06-21 09:57:43 +0700

    🚸 expose pipeline name to module level (#39)
    expose pipeline name to module level

--- tests/test_pystream.py
+++ tests/test_pystream.py
@@ -1,4 +1,5 @@
-from pystream import __version__
+from pystream import __version__, MAIN_PIPELINE_NAME
+from pystream.utils.general import _PIPELINE_NAME_IN_PROFILE
 
 
 def test_version():
@@ -6,5 +7,8 @@ def test_version():
 
 
 def test_import():
-    from pystream import Stage, Pipeline
+    from pystream import Stage, Pipeline, MAIN_PIPELINE_NAME, logger
     from pystream.functional import func_parallel_thread, func_serial
+
+def test_constants():
+    assert MAIN_PIPELINE_NAME == _PIPELINE_NAME_IN_PROFILE
